f638cc775d1587532bfd34937033459fe63003f6
Evans Munene <munene.evansk@gmail.com>
2017-10-26 21:54:22 +0300

    Add: Homeogeneous lists

--- app/Screens/Home.js
+++ app/Screens/Home.js
@@ -1,89 +1,55 @@
 import React, { Component } from "react"
-import { View, Text, StyleSheet, FlatList, Image, ScrollView } from "react-native"
+import { View, Text, StyleSheet, FlatList, Image, SectionList, ScrollView } from "react-native"
 
-const rows = [
+const sections = [
     {
-        "key": 1,
-        "model": "Outlander",
-        "make": "Mitsubishi",
-        "year": 2011,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 2,
-        "model": "Corvette",
-        "make": "Chevrolet",
-        "year": 1960,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 3,
-        "model": "Frontier",
-        "make": "Nissan",
-        "year": 2000,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 4,
-        "model": "Murciélago LP640",
-        "make": "Lamborghini",
-        "year": 2008,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 5,
-        "model": "QX",
-        "make": "Infiniti",
-        "year": 2006,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 6,
-        "model": "Regal",
-        "make": "Buick",
-        "year": 1989,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 7,
-        "model": "Baja",
-        "make": "Subaru",
-        "year": 2005,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 8,
-        "model": "M",
-        "make": "Infiniti",
-        "year": 1992,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 9,
-        "model": "Legacy",
-        "make": "Subaru",
-        "year": 2012,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }, {
-        "key": 10,
-        "model": "Aurora",
-        "make": "Oldsmobile",
-        "year": 2002,
-        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-    }]
+        key: 0,
+        title: 'Basic Components',
+        data: [
+            { key: 0, text: 'View' },
+            { key: 1, text: 'Text' },
+            { key: 2, text: 'Image' },
+            { key: 3, text: 'Image' },
+            { key: 4, text: 'Image' },
+            { key: 5, text: 'Image' },
+            { key: 6, text: 'Image' },
+            { key: 7, text: 'Image' },
+        ]
+    },
+    {
+        key: 1,
+        title: 'List Components',
+        data: [
+            { key: 3, text: 'ScrollView' },
+            { key: 4, text: 'ListView' },
+        ]
+    }
+]
 export class HomeSceen extends Component {
 
-    render() {
-        console.log(rows)
+    renderItem = ({item}) =>{
+        return(
+            <View  style={homeStyles.square}/>
+        )
+    }
+
+    renderSectionHeader = ({section}) => {
+        return(
+            <Text style={homeStyles.header}>
+                {section.title}
+            </Text>
+        )
+    }
+
+    render() {        
         return (
-            <View style={homeStyles.Screen}>
-                <FlatList
-                    data={rows}
-                    renderItem={({ item }) => (
-                        <View style={homeStyles.item}>
-                            <View style={{ flexDirection: "row" }}>
-                                <Text style={homeStyles.header}>{item.model}</Text>
-                                <Text style={homeStyles.date} >{item.make}</Text>
-                            </View>
-                            <View style={{}}>
-                                <Text style={homeStyles.description} ellipsizeMode="tail" numberOfLines={3} >{item.description}</Text>
-                            </View>
-                        </View>
-                    )}
+            <ScrollView style={homeStyles.Screen}>
+                <SectionList 
+                    sections={sections}
+                    renderItem={this.renderItem}
+                    renderSectionHeader={this.renderSectionHeader}
                 />
-            </View>
+            </ScrollView>
         )
     }
 }
@@ -129,19 +95,25 @@ const homeStyles = StyleSheet.create({
     },
     item: {
         paddingTop: 10,
-        paddingBottom: 10,  
+        paddingBottom: 10,
     },
 
     header: {
         fontSize: 22,
         flex: 2
     },
-    description:{
-        
+    description: {
+
     },
     date: {
         flex: 1,
         textAlign: 'right'
+    },
+
+    square: {
+        width: 60,
+        height: 60,
+        backgroundColor: 'blue'
     }
 
 
