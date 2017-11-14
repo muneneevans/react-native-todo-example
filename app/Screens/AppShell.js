707a46ddd939d3a3476cc9b64d67b2c8610c7060
Evans Munene <munene.evansk@gmail.com>
2017-11-14 03:53:09 -0600

    ADD: Navigation in App shell

--- app/Screens/AppShell.js
+++ app/Screens/AppShell.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet, ListView, FlatList, Image, SectionList, ScrollView } from "react-native"
+import { StackNavigator } from "react-navigation"
 
 //import screens
 import TasksScreen from "../Screens/TasksScreen"
+import Home from "../Screens/Home"
 
-class AppShell extends Component {
-    render() {
-        return (
-            <TasksScreen />
-        )
+
+
+const AppShell = StackNavigator(
+    {
+        //the screens to be shown
+        Task: {
+            screen: TasksScreen,
+            header: null
+        },
+        Home: {
+            screen: Home
+        }
+    },
+    {
+        // set the header
+        headerMode: 'none',
     }
-}
+)
+
+
 
 export default AppShell
